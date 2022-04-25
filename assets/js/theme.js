a6a662735d6d24be0522e783f76e50479be2c8b6
GunitaG <gunitagorska@gmail.com>
2022-04-26 01:25:46 +0300

    -

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -8,10 +8,41 @@ $(document).ready(function() {
         }
     });
 
-    // $('li').click(function(){
-    //     $('li').removeClass("active");
-    //     $(this).addClass("active");
-    // });
+    //--- Toggle between bootcamp process sub-lists arrow
+
+    var $bootcamp_process = $('.flex_block_ol');
+    $bootcamp_process.click(function(){
+        $bootcamp_process.removeClass('active_block');
+        $(this).addClass('active_block');
+    });
+
+    // -- Toggle between bootcamp process sub-lists
+    var $bootcamp_first_block = $('#first_active_block');
+    var $bootcamp_first_sublist = $('#first_active_sublist');
+
+    var $bootcamp_second_block = $('#second_active_block');
+    var $bootcamp_second_sublist = $('#second_active_sublist');
+
+    var $bootcamp_third_block = $('#third_active_block');
+    var $bootcamp_third_sublist = $('#third_active_sublist');
+
+    var $bootcamp_sublists = $('.grid_2');
+
+    $bootcamp_first_block.click(function(){
+        $bootcamp_sublists.removeClass('active_sublist');
+        $bootcamp_first_sublist.addClass('active_sublist');
+    });
+
+    $bootcamp_second_block.click(function(){
+        $bootcamp_sublists.removeClass('active_sublist');
+        $bootcamp_second_sublist.addClass('active_sublist');
+    });
+
+    $bootcamp_third_block.click(function(){
+        $bootcamp_sublists.removeClass('active_sublist');
+        $bootcamp_third_sublist.addClass('active_sublist');
+    });
+
 });
 
 document.querySelectorAll('details').forEach((card,_,A)=>{
